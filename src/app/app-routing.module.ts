9f9430e7c37335d4bf267f811da9719a8bd4b77c
Veronika Stanovská <veronika.stanovska@kosickaakademia.sk>
2022-01-14 12:29:25 +0100

    user board html/css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './board/board.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'board', component: BoardComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
 ];
 
