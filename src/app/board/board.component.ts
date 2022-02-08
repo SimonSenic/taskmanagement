425a93b4db5614cd5a39e46e0ac5a2a3d8c7b8d0
SimonSenic <simon.senic@kosickaakademia.sk>
2022-02-08 23:34:42 +0100

    Saving changes into database

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -3,7 +3,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { Router } from '@angular/router';
 import firebase from 'firebase/compat/app';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
@@ -12,36 +12,21 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-  //todo = [''];
-  //done = [''];
 
-  public todo!: Observable<any>[];
-  public doing!: Observable<any>[];
-  public done!: Observable<any>[];
+  todo!: Observable<any[]>;
+  todo_!: AngularFireList<any>;
+  doing!: Observable<any[]>;
+  doing_!: AngularFireList<any>;
+  done!: Observable<any[]>;
+  done_!: AngularFireList<any>;
 
-  constructor(private auth: AngularFireAuth, private router: Router, private db: AngularFireDatabase) {
-    const todo: AngularFireList<any> = db.list('todo');
-    const doing: AngularFireList<any> = db.list('doing');
-    const done: AngularFireList<any> = db.list('done');
-
-    todo.valueChanges().subscribe(
-      x => { this.todo = x; }
-    );
-
-    doing.valueChanges().subscribe(
-      x => { this.doing = x; }
-    );
-
-    done.valueChanges().subscribe(
-      x => { this.done = x; }
-    );
-  }
+  constructor(private auth: AngularFireAuth, private router: Router, private db: AngularFireDatabase) {}
 
   user$ = this.auth.user;
 
-  drop(event: CdkDragDrop<any[]>) {
+  drop(event: CdkDragDrop<any>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      //moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(
         event.previousContainer.data,
@@ -49,7 +34,33 @@ export class BoardComponent implements OnInit {
         event.previousIndex,
         event.currentIndex,
       );
+      //console.log(event.item);
+      //this.getName(event.item);
+      this.update(event.item.element.nativeElement.innerHTML, event.previousContainer, event.container)
     }
+  }
+
+  /*getName(item: any){
+    //console.log(this.todo)
+    for(var i=0; i<10; i++){
+    }
+  }*/
+
+  update(title: string, previousContainer: any, container: any){
+    if(container.data !== previousContainer.data){
+      this.db.list(container.id).set(title, {
+        title: title,
+        description: ""
+      })
+      this.db.list(previousContainer.id).remove(title);
+    }else{ console.log("nejdze")}
+  }
+
+  add(){
+    this.db.list("doing").set('Create component',{
+      title: "Create component",
+      description: "Create new component"
+    });
   }
 
   signOut(){
@@ -57,7 +68,16 @@ export class BoardComponent implements OnInit {
     window.location.reload();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    //this.add();
+    this.todo_ = this.db.list('todo');
+    this.todo = this.todo_.valueChanges();
+
+    this.doing_ = this.db.list('doing');
+    this.doing = this.doing_.valueChanges();
+
+    this.done_ = this.db.list('done');
+    this.done = this.done_.valueChanges();
   }
 
 }
