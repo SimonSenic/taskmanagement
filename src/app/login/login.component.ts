10477d42dca66431b34e2b282baa7fc7aae57395
SimonSenic <simon.senic@kosickaakademia.sk>
2022-05-29 06:18:30 +0200

    validator completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,23 +10,49 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm!: FormGroup;
+  //loginForm!: FormGroup;
+
+  validity = true;
+
+  unauth = false;
 
   constructor(private formBuilder: FormBuilder, private auth: AngularFireAuth, private router: Router) { }
 
+  loginForm = new FormGroup({ 
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+   })
+
   signIn(){
-    this.auth.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password)
-    .then(() => { 
-      console.log("Signed in")
-      this.router.navigate(['']);
-    })
+    if(this.loginForm.valid){
+      this.auth.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password)
+      .then(() => { 
+        console.log("Signed in")
+        this.router.navigate(['']);
+      }).catch(() => {
+        this.unauth=true;
+        console.log("---")
+      })
+      this.validity=true;
+    }else{ 
+      this.unauth = false;
+      this.validity=false; console.log("--") 
+    }
+  
   }
 
+  get email(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+   }  
+   get password(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+   }
+
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
+    /*this.loginForm = this.formBuilder.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    })
+    })*/
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,26 +10,63 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  registerForm!: FormGroup;
+  //registerForm!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private auth: AngularFireAuth, private router: Router) { }
 
+  validity = true;
+
+  unauth = false;
+
+  mismatch = false;
+
+  registerForm = new FormGroup({ 
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    repeat: new FormControl('', [Validators.required])
+   })
+
   signUp(){ //form: NgForm
-    this.auth.createUserWithEmailAndPassword(this.registerForm.value.email, this.registerForm.value.password)
-    .then(() => { 
-      console.log("Signed up");
-      this.router.navigate(['']);
-    })
+    if(this.registerForm.valid && this.password.value===this.repeat.value) {
+      this.auth.createUserWithEmailAndPassword(this.registerForm.value.email, this.registerForm.value.password)
+      .then(() => { 
+        console.log("Signed up");
+        this.router.navigate(['']);
+      }).catch(() => {
+        this.unauth=true;
+        console.log("---")
+      })
+      this.validity=true;
+    }else if(this.registerForm.valid && this.password.value!=this.repeat.value){ 
+      this.mismatch=true; 
+      this.unauth=false;
+      this.validity=true;
+    }else{ 
+      this.unauth=false;
+      this.validity=false; console.log("--") 
+    }
+    
     //form.resetForm();
     //console.log(this.registerForm.value.email)
    };
 
+   get email(): FormControl {
+    return this.registerForm.get('email') as FormControl;
+   }  
+   get password(): FormControl {
+    return this.registerForm.get('password') as FormControl;
+   }
+   get repeat(): FormControl {
+    return this.registerForm.get('repeat') as FormControl;
+   }
+
+
   ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
+    /*this.registerForm = this.formBuilder.group({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
       repeat: new FormControl('', [Validators.required])
-    })
+    })*/
   }
 
 
